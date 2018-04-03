0126a0792d7750275598abfff42419686d3ab8e7
Dante Soares <dante.m.soares@rice.edu>
2018-04-03 15:06:33 -0500

    JSON API schema now actually validates properly

--- app/serializers/concerns/json_api_schema.rb
+++ app/serializers/concerns/json_api_schema.rb
@@ -4,28 +4,25 @@ module JsonApiSchema
   class_methods do
     def json_schema(create: false)
       humanized_name = name.demodulize.chomp('Serializer').underscore.humanize
-      json_api_schema_filename = create ? 'json_api_create' : 'json-api'
-      json_api_schema_filepath = File.join(
-        Rails.root, 'public', "#{json_api_schema_filename}.schema.json"
-      )
+      attributes = attributes_to_serialize.keys
+      relationships = relationships_to_serialize.keys
+
+      # Load the generic json-api schema
+      json_api_schema_filepath = File.join Rails.root, 'public', 'json-api.schema.json'
       json_api_schema_file = File.open json_api_schema_filepath
       json_api_schema = JSON.parse json_api_schema_file
 
-      {
-        "$schema": "http://json-schema.org/draft-06/schema#",
-        title: "#{humanized_name} JSON API Schema",
-        description: "JSON API schema for #{humanized_name} objects.",
-        allOf: [
-          json_api_schema,
-          {
-            definitions: {
-              resource:      { properties:    { id: { format: :uuid } } },
-              attributes:    { propertyNames: { enum: attributes_to_serialize.keys } },
-              relationships: { propertyNames: { enum: relationships_to_serialize.keys } }
-            }
-          }
-        ]
-      }.to_json
+      # Customize the schema for the current model
+      json_api_schema['$schema'] = 'http://json-schema.org/draft-06/schema#'
+      json_api_schema['title'] = "#{humanized_name} JSON API Schema"
+      json_api_schema['description'] = "JSON API schema for #{humanized_name} objects."
+      json_api_schema['definitions']['resource']['properties']['id']['format'] = 'uuid'
+      json_api_schema['definitions']['resource']['required'] = [ 'type' ] if create
+      json_api_schema['definitions']['attributes']['propertyNames'] = { 'enum' => attributes }
+      json_api_schema['definitions']['relationships']['propertyNames'] = { 'enum' => relationships }
+      json_api_schema['definitions']['linkage']['properties']['id']['format'] = 'uuid'
+
+      json_api_schema.to_json
     end
   end
 end
